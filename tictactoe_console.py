7e7924c12592d7237e1984dfcee96e6fe8c2f28f
Olena Gul <44442087+Developress@users.noreply.github.com>
2020-06-01 23:05:37 +0300

    hw_2_fixed

--- tictactoe_console.py
+++ tictactoe_console.py
@@ -1,96 +1,157 @@
 import random
 
-def showGameboard():
+
+def show_game_board():
+    """Print the game_board on the screen"""
     print("-------------")
     for i in range(3):
-        print("|", gameboard[0 + i * 3], "|", gameboard[1 + i * 3], "|", gameboard[2 + i * 3], "|")
+        print("|", game_board[0 + i * 3], "|", game_board[1 + i * 3], "|", game_board[2 + i * 3], "|")
     print("-------------")
 
-def makeMove():
+
+def make_move():
+    """
+        Let the computer or the user make the move
+        :raises ValueError: when the input is incorrect
+    """
     global mode
     global move
     global player
-    global computerMove
-    if mode == 1 and computerMove:
+    global computer_move
+
+    # if the user plays with the computer and computer needs to move
+    if mode == 1 and computer_move:
         print("Computer moves: ")
+        # generates the random move
         move = random.randint(1, 9)
-        while gameboard[move - 1] == "X" or gameboard[move - 1] == "O":
+
+        # if the field is occupied, generates the random move again
+        while game_board[move - 1] == "X" or game_board[move - 1] == "O":
             move = random.randint(1, 9)
-        computerMove = False
+
+        computer_move = False
+
+    # the user makes the move
     else:
         print(player, "moves: ")
-        move = int(input())
-        if mode == 1:
-            computerMove = True
-    if move in range(1,10) and gameboard[move-1] != "X" and gameboard[move-1] != "O":
-        gameboard[move-1] = player
-    else:
-        print("Invalid move! Try again")
-        makeMove()
-        if mode == 1:
-            computerMove = False
 
-def checkForWin():
-    for i in winCombinations:
+        # loop until the input is correct
+        while True:
+            try:
+                move = int(input())
+                break
+            except ValueError:
+                print("Invalid input! Please, make another move")
+
+        # if the user plays with the computer, then let the computer move next
+        if mode == 1:
+            computer_move = True
+
+    # if the input is correct and field is not occupied, then occupy it
+    if move in range(1, 10) and game_board[move - 1] != "X" and game_board[move - 1] != "O":
+        game_board[move - 1] = player
+
+    # if the field is already occupied
+    else:
+        print("The field is occupied! Please, make another move")
+
+        # if the user plays with the computer, then cancel the computer move
+        # to let the user move again
+        if mode == 1:
+            computer_move = False
+
+        # make one more move
+        make_move()
+
+
+def check_for_win():
+    """
+        Check all the win combinations to confirm the victory
+        :return: True if the victory is confirmed, False if not
+    """
+
+    for i in win_combinations:
+        win = False
+
         for j in i:
-           if gameboard[j-1] != player:
-               win = False
-               break
-           else:
-               win = True
+            # if the field from the win combination matches with the player
+            # then assign win to True
+            if game_board[j - 1] == player:
+                win = True
+
+            # if the field from the win combination does not match with the player
+            # then stop checking the current combination and check the next one
+            else:
+                win = False
+                break
+
+        # if the loop has not been breaking, then the victory is confirmed
         if win:
             return True
+
+    # if the loop has been breaking all the time, then the victory is not confirmed
     return False
 
 
-gameboard = list(range(1, 10))
-move = -1
+# initialize everything before the start of the game
+game_board = list(range(1, 10))
+move = 0
+counter = 0
 player = "X"
 extra = "O"
-mode = int(input("Choose a mode to play:\n"
-             "[1] - with computer\n"
-             "[2] - two players\n"))
+computer_move = False
+win_combinations = ((1, 2, 3), (4, 5, 6), (7, 8, 9), (1, 4, 7), (2, 5, 8), (3, 6, 9),
+                    (1, 5, 9), (3, 5, 7))
 
-while not (mode in range(1,3)):
-    print("Invalid input. Try again!")
-    mode = int(input("Choose a mode to play:\n"
-                 "[1] - with computer\n"
-                 "[2] - two players\n"))
+print("Welcome to the tic tac toe game!")
 
-if mode == 1:
-    choise = int(input("Choose a player:\n"
-                 "X - [1]\n"
-                 "O - [2]\n"))
-
-    while choise!=1 and choise!=2:
+# loop until the input is correct
+while True:
+    try:
+        mode = int(input("Choose a mode to play:\n"
+                         "[1] - with computer\n"
+                         "[2] - two players\n"))
+        while not (mode in range(1, 3)):
+            print("Invalid input. Try again!")
+            mode = int(input("Choose a mode to play:\n"
+                             "[1] - with computer\n"
+                             "[2] - two players\n"))
+        break
+    except ValueError:
         print("Invalid input! Try again")
-        player = int(input("Choose a player:\n"
-                 "X - [1]\n"
-                 "O - [2]\n"))
 
-    if choise == 1:
-        computerMove = False
-    else:
-        computerMove = True
+# if the user plays with the computer, then ask him whether he requires the first move
+if mode == 1:
+    choice = input("Do you require the first move? Enter y if yes, another key if no\n")
 
-showGameboard()
-winCombinations = ((1,2,3),(4,5,6),(7,8,9), (1,4,7),(2,5,8),(3,6,9),(1,5,9),
-                   (3,5,7))
-i = 0
+    # if the input is different from "y", then the user does not require the first move
+    # so the computer moves first
+    if choice != "y":
+        computer_move = True
+
+print("\nTo make the move, please enter the appropriate number, "
+      "which corresponds to the position of the field")
+show_game_board()
 
 while True:
-    makeMove()
-    showGameboard()
+    make_move()
+    show_game_board()
 
-    if checkForWin():
-        if mode == 1 and not computerMove:
+    if check_for_win():
+        # if the user plays with the computer and computer has moved recently
+        if mode == 1 and not computer_move:
             print("Computer won!")
+        # if the user has moved recently
         else:
             print(player, "won!")
         break
 
-    i+=1
-    if i == 9:
+    counter += 1
+
+    # if all the fields are occupied, then confirm the draw
+    if counter == 9:
         print("Draw")
         break
+
+    # change the active player
     player, extra = extra, player
--- tictactoe_with ui.py
+++ tictactoe_with ui.py
@@ -1,131 +1,162 @@
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QWidget, QGridLayout, QSizePolicy, QMessageBox
 import sys
 import random
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QWidget, QGridLayout, QSizePolicy, \
+                            QMessageBox
+
 
 class MainWindow(QMainWindow):
+    """Create the main window"""
     def __init__(self):
         super(MainWindow, self).__init__()
+
+        # initialize everything before the start of the game
+        self.counter = 0
+        self.win_combinations = ((1, 2, 3), (4, 5, 6), (7, 8, 9), (1, 4, 7), (2, 5, 8), (3, 6, 9), (1, 5, 9),
+                                 (3, 5, 7))
+        self.game_board = list(range(1, 10))
+        self.computer_move = False
+        self.mode = -1
+        self.extra = "O"
+        self.player = "X"
+        self.central_widget = QWidget()
+        self.btn_o = QPushButton(self.central_widget)
+        self.btn_two_players = QPushButton(self.central_widget)
+        self.btn_x = QPushButton(self.central_widget)
+        self.btn_one_player = QPushButton(self.central_widget)
+        self.label = QLabel(self.central_widget)
+        self.buttons = {}
+        self.layout = QGridLayout(self.central_widget)
         self.resize(800, 600)
         self.setWindowTitle("Tic Tac Toe")
-        self.initUi()
+        self.init_ui()
 
-    def initUi(self):
-        self.centralWidget = QWidget()
-        self.setCentralWidget(self.centralWidget)
-        self.centralWidget.setStyleSheet("background-color: rgb(230, 230, 230);\n")
-        self.layout = QGridLayout(self.centralWidget)
-        self.buttons = {}
+    def init_ui(self):
+        """Initialize the elements of UI"""
+        self.setCentralWidget(self.central_widget)
+        self.central_widget.setStyleSheet("background-color: rgb(230, 230, 230);\n")
+
+        # initialize the grid of buttons
         for i in range(0, 3):
             for j in range(0, 3):
-                self.buttons[3*i + j] = QPushButton(self.centralWidget)
-                self.buttons[3*i + j].setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Expanding)
-                self.buttons[3*i + j].setStyleSheet("background-color: white;\n"
-                    "color: rgb(130, 130, 130);\n"
-                    "font: 35pt \"Segoe UI\";\n"
-                    "border-width: 0px;\n"
-                    "border-style: solid;\n"
-                    " border-radius: 10px;")
-                self.layout.addWidget(self.buttons[3*i + j], i, j)
-                self.buttons[3*i + j].hide()
-        self.label = QLabel(self.centralWidget)
-        self.label.setText("Сhoose option")
+                self.buttons[3 * i + j] = QPushButton(self.central_widget)
+                self.buttons[3 * i + j].setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Expanding)
+                self.buttons[3 * i + j].setStyleSheet("background-color: white;\n"
+                                                      "color: rgb(130, 130, 130);\n"
+                                                      "font: 35pt \"Segoe UI\";\n"
+                                                      "border-width: 0px;\n"
+                                                      "border-style: solid;\n"
+                                                      " border-radius: 10px;")
+                self.layout.addWidget(self.buttons[3 * i + j], i, j)
+                self.buttons[3 * i + j].hide()
+
+        self.label.setText("Chose option")
         self.label.setGeometry(290, 140, 200, 50)
         self.label.setStyleSheet(u"font: 16pt \"Segoe UI\";")
-        self.btnOnePlayer = QPushButton(self.centralWidget)
-        self.btnOnePlayer.setText("1 player")
-        self.btnOnePlayer.setGeometry(290, 210, 160, 50)
-        self.btnOnePlayer.setStyleSheet(
+        self.btn_one_player.setText("1 player")
+        self.btn_one_player.setGeometry(290, 210, 160, 50)
+        self.btn_one_player.setStyleSheet(
             u"background-color: rgb(130, 130, 130);\n"
             "color: white;\n"
             "font: 12pt \"Segoe UI\";\n"
             "border-width: 0px;\n"
             "border-style: solid;\n"
             " border-radius: 10px;")
-        self.btnX = QPushButton(self.centralWidget)
-        self.btnX.setText("X")
-        self.btnX.setGeometry(290, 210, 160, 50)
-        self.btnX.setStyleSheet(
+        self.btn_x.setText("X")
+        self.btn_x.setGeometry(290, 210, 160, 50)
+        self.btn_x.setStyleSheet(
             u"background-color: rgb(130, 130, 130);"
             "color: white;\n"
             "font: 12pt \"Segoe UI\";\n"
             "border-width: 0px;\n"
             "border-style: solid;\n"
             " border-radius: 10px;")
-        self.btnX.hide()
-        self.btnTwoPlayers = QPushButton(self.centralWidget)
-        self.btnTwoPlayers.setText("2 players")
-        self.btnTwoPlayers.setGeometry(290, 280, 160, 50)
-        self.btnTwoPlayers.setStyleSheet(
+        self.btn_x.hide()
+        self.btn_two_players.setText("2 players")
+        self.btn_two_players.setGeometry(290, 280, 160, 50)
+        self.btn_two_players.setStyleSheet(
             u"background-color: rgb(130, 130, 130);\n"
             "color: white;\n"
             "font: 12pt \"Segoe UI\";\n"
             "border-width: 0px;\n"
             "border-style: solid;\n"
             " border-radius: 10px;")
-        self.btnO = QPushButton(self.centralWidget)
-        self.btnO.setText("O")
-        self.btnO.setGeometry(290, 280, 160, 50)
-        self.btnO.setStyleSheet(
+        self.btn_o.setText("O")
+        self.btn_o.setGeometry(290, 280, 160, 50)
+        self.btn_o.setStyleSheet(
             u"background-color: rgb(130, 130, 130);\n"
             "color: white;\n"
             "font: 12pt \"Segoe UI\";\n"
             "border-width: 0px;\n"
             "border-style: solid;\n"
             " border-radius: 10px;")
-        self.btnO.hide()
-        self.btnOnePlayer.clicked.connect(self.btnOnePlayerClicked)
-        self.btnTwoPlayers.clicked.connect(self.btnTwoPlayersClicked)
-        self.btnX.clicked.connect(self.btnXClicked)
-        self.btnO.clicked.connect(self.btnOClicked)
-        self.player = "X"
-        self.extra = "O"
-        self.mode = -1
-        self.computerMove = False
-        self.gameboard = list(range(1, 10))
-        self.winCombinations = ((1, 2, 3), (4, 5, 6), (7, 8, 9), (1, 4, 7), (2, 5, 8), (3, 6, 9), (1, 5, 9),
-                           (3, 5, 7))
+        self.btn_o.hide()
+
+        # connect the buttons to on click functions
+        self.btn_one_player.clicked.connect(self.btn_one_player_clicked)
+        self.btn_two_players.clicked.connect(self.btn_two_players_clicked)
+        self.btn_x.clicked.connect(self.btn_x_clicked)
+        self.btn_o.clicked.connect(self.btn_o_clicked)
+
         for i in range(0, 3):
             for j in range(0, 3):
-                self.buttons[3*i + j].clicked.connect(self.makeMove)
-        self.counter = 0
+                self.buttons[3 * i + j].clicked.connect(self.make_move)
 
-    def btnOnePlayerClicked(self):
-        self.btnOnePlayer.hide()
-        self.btnTwoPlayers.hide()
+    def btn_one_player_clicked(self):
+        """Hide buttons for one player and two. Show the X and O buttons"""
+        self.btn_one_player.hide()
+        self.btn_two_players.hide()
         self.mode = 1
-        self.btnX.show()
-        self.btnO.show()
-    def btnTwoPlayersClicked(self):
-        self.btnOnePlayer.hide()
-        self.btnTwoPlayers.hide()
+        self.btn_x.show()
+        self.btn_o.show()
+
+    def btn_two_players_clicked(self):
+        """Hide buttons for one player and two. Start the game"""
+        self.btn_one_player.hide()
+        self.btn_two_players.hide()
         self.mode = 2
-        self.startGame()
-    def btnXClicked(self):
-        self.computerMove = False
-        self.startGame()
-    def btnOClicked(self):
-        self.computerMove = True
-        self.startGame()
-    def startGame(self):
+        self.start_game()
+
+    def btn_x_clicked(self):
+        """Let the user make the first move. Start the game"""
+        self.computer_move = False
+        self.start_game()
+
+    def btn_o_clicked(self):
+        """Let the computer make the first move. Starts the game"""
+        self.computer_move = True
+        self.start_game()
+
+    def start_game(self):
+        """Hide all the buttons, display the field and start the game"""
         self.label.hide()
-        self.btnX.hide()
-        self.btnO.hide()
+        self.btn_x.hide()
+        self.btn_o.hide()
+
+        # display the grid of buttons
         for i in range(0, 3):
             for j in range(0, 3):
                 self.buttons[3 * i + j].setText("")
-                self.buttons[3*i + j].show()
-        if self.mode == 1 and self.computerMove:
-            self.makeComputerMove()
+                self.buttons[3 * i + j].show()
 
-    def makeMove(self):
-        self.counter+=1
+        # if the user plays with the computer and computer needs to move
+        if self.mode == 1 and self.computer_move:
+            self.make_computer_move()
+
+    def make_move(self):
+        """Perform the user's move and check the situation on the field"""
+        self.counter += 1
+
+        # get the button which was clicked
         sender = self.sender()
+
+        # if the field is not occupied, then occupy it
         if sender.text() != "X" and sender.text() != "O":
             sender.setText(self.player)
         else:
             return
-        if self.checkForWin():
+
+        # if the victory is confirmed, then form a message box and exit the game
+        if self.check_for_win():
             msg = QMessageBox()
             msg.setIcon(QMessageBox.Information)
             string = "Congratulations! " + self.player + " won!"
@@ -133,6 +164,9 @@ class MainWindow(QMainWindow):
             msg.setWindowTitle("Victory!")
             msg.exec()
             self.hide()
+
+        # if all the fields are occupied, then confirm the draw,
+        # form a message box and exit the game
         elif self.counter == 9:
             msg = QMessageBox()
             msg.setIcon(QMessageBox.Information)
@@ -140,36 +174,66 @@ class MainWindow(QMainWindow):
             msg.setWindowTitle("Draw!")
             msg.exec()
             self.hide()
-        self.player, self.extra = self.extra, self.player
-        if self.mode == 1:
-            self.computerMove = True
-            self.makeComputerMove()
-    def makeComputerMove(self):
-        move = random.randint(1, 9)
-        while self.buttons[move - 1].text() == "X" or self.buttons[move - 1].text() == "O":
-            move = random.randint(1, 9)
-        self.computerMove = False
-        self.counter += 1
-        self.buttons[move-1].setText(self.player)
+
+        # change the active player
         self.player, self.extra = self.extra, self.player
 
-    def checkForWin(self):
-        for i in self.winCombinations:
+        # if the user plays with the computer and computer needs to move
+        if self.mode == 1:
+            self.computer_move = True
+            self.make_computer_move()
+
+    def make_computer_move(self):
+        """Generate the random move and perform it"""
+        move = random.randint(1, 9)
+
+        # if the field is occupied, generates the random move again
+        while self.buttons[move - 1].text() == "X" or self.buttons[move - 1].text() == "O":
+            move = random.randint(1, 9)
+
+        self.computer_move = False
+        self.counter += 1
+
+        # perform move
+        self.buttons[move - 1].setText(self.player)
+
+        # change the active player
+        self.player, self.extra = self.extra, self.player
+
+    def check_for_win(self):
+        """
+            Check all the win combinations to confirm the victory
+            :return: True if the victory is confirmed, False if not
+        """
+        for i in self.win_combinations:
+            win = False
+
+            # if the field from the win combination matches with the player
+            # then assign win to True
             for j in i:
-                if self.buttons[j-1].text() != self.player:
+                if self.buttons[j - 1].text() == self.player:
+                    win = True
+
+                # if the field from the win combination does not match with the player
+                # then stop checking the current combination and check the next one
+                else:
                     win = False
                     break
-                else:
-                    win = True
+
+            # if the loop has not been breaking, then the victory is confirmed
             if win:
                 return True
+
+        # if the loop has been breaking all the time, then the victory is not confirmed
         return False
 
 
 def window():
+    """Create the instance of the window"""
     app = QApplication(sys.argv)
     win = MainWindow()
     win.show()
     sys.exit(app.exec_())
+
 
 window()
